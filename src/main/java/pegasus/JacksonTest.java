6b522f65b8a606ad414ab0f85158d12c893b9277
LXIV <themrsung@gmail.com>
2023-12-29 03:36:11 +0900

    Update JacksonTest.java

--- src/main/java/pegasus/JacksonTest.java
+++ src/main/java/pegasus/JacksonTest.java
@@ -77,3 +77,43 @@ public class JacksonTest {
     // Don't run this on your desktop lol
     private static final String FILEPATH = "test.yml";
 }
+
+/**
+
+OK I'll try to explain what's going on. I'm not waiting for IntelliJ to index just to edit this file, so I'm doing it on GitHub web UI. Forgive any typos.
+
+During the static initialization of the class JacksonTest, a YAMLMapper named MAPPER is created. (Line 75)
+A constant FILEPATH is also defined. (Line 78)
+
+Any code can now call either JacksonTest::save or JacksonTest::load. Both methods delegate the IOException to the outer scope. (Meaning you'll have to try-catch)
+See the two runnable main methods for how they're used.
+
+Now about the Person class. An arbitrary data type was used to demonstrate Jackson's abilities to serialize and deserialize custom objects.
+
+You can either:
+
+A)
+- Add an all-args constructor (it can be private) for *every single variable* of the data class. If the class contains another object as a variable,
+the object should have an all-args constructor as well. (until a primitive type or things like String, UUID are reached)
+- Do NOT override Object::toString.
+- Pray to the Java gods.
+- NEVER add a variable EVER.
+- Put @JsonIgnore on all methods.
+
+B)
+- Override Object::toString to a format of your choice.
+- Understand how Jackson handles deserialization, and it's limitations.
+
+When given serialized data and a class, Jackson will first look for custom deserializers defined by the dev.
+If no deserializer was designated it will parse the text and look for an all-args constructor with a matching signature.
+For example, when given ** {"type": "DOG", "age": 1} ** it will look for a constructor with the signature of (String, Number). <- I think it's in the order of int, long, double but you'll have to check idk
+
+If it cannot find one, it will give up and throw an error. If you can understand that error, why are you reading this lol.
+If not, search Google for "custom deserializer jackson yaml" or something. Or just ask the GPT god for a working example.
+
+In the highly improbable event that my explanation is imperfect, clone this repo and send me a pull request so I can push one button and fix it.
+Else I will do nothing.
+
+Happy hacking! (I hate React)
+
+*/
